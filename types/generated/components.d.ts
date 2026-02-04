e1c6b62daeaea184320f78a5c225002a6cc17328
ad-luver <259377249+ad-luver@users.noreply.github.com>
2026-02-04 17:49:59 +0100

    initial commit

---
+++ types/generated/components.d.ts
@@ -0,0 +1,66 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface CmsButtonLink extends Struct.ComponentSchema {
+  collectionName: 'components_cms_button_links';
+  info: {
+    displayName: 'ButtonLink';
+  };
+  attributes: {
+    href: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        minLength: 1;
+      }>;
+    size: Schema.Attribute.Enumeration<['default', 'sm', 'lg']> &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'default'>;
+    value: Schema.Attribute.String & Schema.Attribute.Required;
+    variant: Schema.Attribute.Enumeration<
+      ['default', 'secondary', 'outline', 'ghost', 'link', 'destructive']
+    > &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'default'>;
+  };
+}
+
+export interface CmsImage extends Struct.ComponentSchema {
+  collectionName: 'components_cms_images';
+  info: {
+    displayName: 'Image';
+  };
+  attributes: {
+    alt: Schema.Attribute.String & Schema.Attribute.Required;
+    value: Schema.Attribute.Media<'images'> & Schema.Attribute.Required;
+  };
+}
+
+export interface CmsParagraph extends Struct.ComponentSchema {
+  collectionName: 'components_cms_paragraphs';
+  info: {
+    displayName: 'Paragraph';
+  };
+  attributes: {
+    value: Schema.Attribute.Text & Schema.Attribute.Required;
+  };
+}
+
+export interface CmsRichTextParagraph extends Struct.ComponentSchema {
+  collectionName: 'components_cms_rich_text_paragraphs';
+  info: {
+    displayName: 'RichTextParagraph';
+  };
+  attributes: {
+    value: Schema.Attribute.Blocks & Schema.Attribute.Required;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'cms.button-link': CmsButtonLink;
+      'cms.image': CmsImage;
+      'cms.paragraph': CmsParagraph;
+      'cms.rich-text-paragraph': CmsRichTextParagraph;
+    }
+  }
+}
